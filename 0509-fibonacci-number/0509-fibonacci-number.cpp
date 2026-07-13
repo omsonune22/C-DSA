15a6cd5f7e1cd4b5880f20f9704d882e5ecd5a8e
Om Sonune <omsonune1122@gmail.com>
2026-07-13 23:33:26 +0530

    Time: 13 ms (32.29%), Space: 7.7 MB (65.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-
-        if (n == 0)
-            return 0;
-
-        if (n == 1)
-            return 1;
-
-        int prev2 = 0;
-        int prev1 = 1;
-
-        for (int i = 2; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+       if(n<=1){
+        return n;
         }
-
-        return prev1;
+        return fib(n-1)+fib(n-2); 
     }
 };
